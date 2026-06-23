468ac8c368f17508ff74cfadbcef0fdd190b8279
Anushka Yadav <ay0496618@gmail.com>
2026-06-23 22:24:26 +0530

    Create Hello_World.java

---
+++ 01-BASIC/Hello_World.java
@@ -0,0 +1,15 @@
+// This is my 1st java program.
+public class Hello_World 
+{ // Starts the Hello_World class container
+
+// The starting point where code execution begins.
+public static void main(String args[])
+
+{// Starts the main method instructions.
+
+ // Prints "Hello World!" on the screen.
+System.out.println("Hello World!");   
+
+
+} // Ends the main method instructions.
+}// Ends the Hello_World class container.
