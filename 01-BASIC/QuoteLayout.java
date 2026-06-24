944c445918754e2458cab09044cfa5a768b4e6f6
Anushka Yadav <ay0496618@gmail.com>
2026-06-24 22:54:37 +0530

    Add files via upload

---
+++ 01-BASIC/QuoteLayout.java
@@ -0,0 +1,10 @@
+class QuoteLayout
+{
+public static void main(String[] args) 
+{
+System.out.println("YOU CANNOT DEFEAT A PERSON");
+System.out.println("WHO IS HAVING FUN.");
+System.out.println(" ");
+System.out.println("-By Unknown");
+}
+}
