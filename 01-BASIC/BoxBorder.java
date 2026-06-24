189519b1df9d092c7c4e04b19de03217e0aec751
Anushka Yadav <ay0496618@gmail.com>
2026-06-24 20:13:21 +0530

    Add files via upload

---
+++ 01-BASIC/BoxBorder.java
@@ -0,0 +1,13 @@
+class BoxBorder
+{
+public static void main(String args[])
+{
+System.out.println("###############");
+System.out.println("#             #");
+System.out.println("#             #");
+System.out.println("#   ANUSHKA   #");
+System.out.println("#             #");
+System.out.println("#             #");
+System.out.println("###############");
+}
+}
