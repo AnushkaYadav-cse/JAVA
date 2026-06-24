61d6eef8fd443e162e97ef9b700b98c1d0fdc4a9
Anushka Yadav <ay0496618@gmail.com>
2026-06-24 21:19:46 +0530

    Update ASCII_Art.java

--- 01-BASIC/ASCII_Art.java
+++ 01-BASIC/ASCII_Art.java
@@ -4,8 +4,8 @@ public static void main(String args[])
 {
 System.out.println("    /\\");
 System.out.println("   /  \\");
-System.out.println("  /___\\");
-System.out.println(" |       |");
-System.out.println(" |_______|");
+System.out.println("  /____\\");
+System.out.println("  |     |");
+System.out.println("  |_____|");
 }
 }
