b7409caa892cb36067c91f2c26aa4d7c156e40c8
Anushka Yadav <ay0496618@gmail.com>
2026-06-24 19:35:50 +0530

    Add files via upload

---
+++ 01-BASIC/BioData.java
@@ -0,0 +1,9 @@
+class BioData
+{
+public static void main(String[] args)
+{
+System.out.println("My name is Anushka.");
+System.out.println("My college is XYZ Institute.");
+System.out.println("My department is CSE.");
+}
+}
